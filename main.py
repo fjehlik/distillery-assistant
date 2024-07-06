b1bd4d7aa8fb9a1f5d6ca3f15fcbf369f07ed27c
Forrest Jehlik <fjehlik@gmail.com>
2024-07-06 12:24:12 -0400

    update branch to fix css file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import List
 import uvicorn
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
