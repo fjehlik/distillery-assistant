8661b3bd0b9348bac2c411bbc0a0b348a9030892
Forrest Jehlik <fjehlik@gmail.com>
2024-07-07 09:52:45 -0400

    separate dictionary from main.py code and import

--- main.py
+++ main.py
@@ -4,41 +4,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import List
 import uvicorn
+from grain_ppg import grain_ppg  # Import the dictionary
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-
-# Dictionary mapping grain types to their Max PPG and Typical PPG values
-grain_ppg = {
-    "2 Row Barley": {"Max PPG": 37, "Typical PPG": 31},
-    "6 Row Barley": {"Max PPG": 35, "Typical PPG": 30},
-    "Biscuit/Victory Malt": {"Max PPG": 35, "Typical PPG": 30},
-    "Vienna Malt": {"Max PPG": 35, "Typical PPG": 30},
-    "Munich Malt": {"Max PPG": 35, "Typical PPG": 30},
-    "Brown Malt": {"Max PPG": 32, "Typical PPG": 28},
-    "Dextrin Malt": {"Max PPG": 32, "Typical PPG": 28},
-    "Light Crystal (10 - 15L)": {"Max PPG": 35, "Typical PPG": 30},
-    "Pale Crystal (25 - 40L)": {"Max PPG": 34, "Typical PPG": 29},
-    "Medium Crystal (60 - 75L)": {"Max PPG": 34, "Typical PPG": 29},
-    "Dark Crystal (120L)": {"Max PPG": 33, "Typical PPG": 28},
-    "Special B": {"Max PPG": 31, "Typical PPG": 27},
-    "Chocolate Malt": {"Max PPG": 28, "Typical PPG": 24},
-    "Roast Barley": {"Max PPG": 25, "Typical PPG": 22},
-    "Black Patent Malt": {"Max PPG": 25, "Typical PPG": 22},
-    "Wheat Malt": {"Max PPG": 37, "Typical PPG": 31},
-    "Rye Malt": {"Max PPG": 29, "Typical PPG": 25},
-    "Cracked Corn": {"Max PPG": 30, "Typical PPG": None},
-    "Oatmeal (Flaked)": {"Max PPG": 32, "Typical PPG": 28},
-    "Corn (Flaked)": {"Max PPG": 39, "Typical PPG": 33},
-    "Barley (Flaked)": {"Max PPG": 32, "Typical PPG": 28},
-    "Wheat (Flaked)": {"Max PPG": 36, "Typical PPG": 30},
-    "Rice (Flaked)": {"Max PPG": 38, "Typical PPG": 32},
-    "Malto-Dextrin Powder": {"Max PPG": 40, "Typical PPG": 40},
-    "Sugar (Corn, Cane)": {"Max PPG": 46, "Typical PPG": 46}
-}
 
 @app.get("/", response_class=HTMLResponse)
 async def get_form(request: Request):
